e1538c81814b085afba02bbe68d0fe059ad356da
dianesaia <pontesaia@gmail.com>
2020-11-01 14:56:20 -0500

    Add Copy Content for About and Features page

--- src/components/aboutComponent.js
+++ src/components/aboutComponent.js
@@ -8,9 +8,22 @@ class About extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<h1>
-					This is the about component
-				</h1>
+						<div>
+							<div>
+								<h1>About</h1>
+								<p>
+								Families come in all sizes, colors, cultures and traditions!  Families who share their values, and traditions with each generation are bound closer together through their common family histories. Reading stories written directly by family members is like reading letters from home. These personal memories offer perspectives on “Who’s who”, and the “Why’s” and “When’s” of family life. Family Library offers a convenient way to share precious family stories with family members no matter where they are in the world.
+								</p>
+							</div>
+							<div>
+								<ul>
+									<li>(holding  hands icon)Family Library brings families closer together, even when separated, and memories come back to life in story form</li>
+									<li>(pencil icon)Family “members” post stories, old photos or recipes for generations to enjoy.</li>
+									<li>(share icon)Browse, read, export, print, or share, via email, links to those special family events and milestones.</li>
+									<li>(heart icon)Memories of  precious experiences of “old time” family photo albums, letters from Grandpa, or family favorite recipes! </li>
+								</ul>
+							</div>
+						</div>
 			</React.Fragment>
 		);
 	}
--- src/components/featuresComponent.js
+++ src/components/featuresComponent.js
@@ -8,7 +8,18 @@ class Features extends Component {
     render() { 
         return (
 			<React.Fragment>
-				<h1>This is the features component</h1>
+				<div>
+                    <h1>Features</h1>
+                    <ul>
+                        <li>(account icon)Archive family stories, histories, memories, photos, videos and recipes in secure family group accounts.</li>
+                        <li>(road icon)Follow stories of family heritage and legacies, or add your own families' journey.</li>
+                        <li>(safety icon)Keep family folklore in a sacred space where safety-checked content is posted for families to enjoy.</li>
+                        <li>(images icon)Preserve those curated daily visuals that you text to mom, dad, and siblings. ( images icon)</li>
+                        <li>(movies icon)Tag content for creating family movies for online get-togethers.</li>
+                        <li>(books icon)Exporting for creating those wonderful story-books or family-photo-books for gift-giving.</li>
+                        <li>(groups icon)Join Family Groups where you can browse or post on multiple family lineages.</li>
+                        </ul>
+                </div>
 			</React.Fragment>
 		);
     }
