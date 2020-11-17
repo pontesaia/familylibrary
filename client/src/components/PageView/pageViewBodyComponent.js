9e6366a8048c63b8a5be13580321e16154df1f6b
Brandon Leon <bsleon@umich.edu>
2020-11-16 21:39:28 -0500

    PostViewBody read and write transitions

--- client/src/components/PageView/pageViewBodyComponent.js
+++ client/src/components/PageView/pageViewBodyComponent.js
@@ -4,93 +4,125 @@ import { Container, Row, Col, Button } from "reactstrap";
 class PageViewBody extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			readFlag: false,
+			writeFlag: false,
+		};
 	}
+
+	toggleRead = () => {
+		this.setState({
+			writeFlag: false,
+			readFlag: !this.state.readFlag,
+		});
+	};
+
+	toggleWrite = () => {
+		this.setState({ readFlag: false, writeFlag: !this.state.writeFlag });
+	};
+
 	render() {
+		let renderData;
 		let data = [];
 		for (let i = 0; i < 10; ++i) {
 			data.push({ id: i });
 		}
-		const renderData = data.map((d) => (
-			<span key={d.id}>
-				<hr
-					className="horizRule mb-5 mt-4 px-0 mx-0"
-					style={styles.horizRule}
-				/>
-				<Container>
-					<Row className="ml-2">
-						<Col xs="12" lg="1" className="pr-2 px-0 text-lg-center">
-							<img
-								src="/images/Ellipse2.png"
-								style={styles.avatar}
-								className="mb-2"
-							/>
-						</Col>
-						<Col className="px-0">
-							<h4>My Father Patrick McCarthy's Garden</h4>
-							<h6>Posted By: Maria McCarthy</h6>
-							<h6>Date posted: June 10, 2020</h6>
-							<h6 className="mb-4">
-								Memories | Folklore | Father
-							</h6>
-						</Col>
-					</Row>
-					<Row className="ml-2 mr-5">
-						<Col xs="12" lg="11" className="px-0 offset-lg-1">
-							<p>
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit. Vivamus velit ligula, vehicula
-								eu egestas at, bibendum eget ligula. Mauris
-								vehicula mattis ultrices. Nulla auctor risus
-								nulla, quis dictum nulla semper nec. Curabitur
-								nec orci ex. Integer viverra augue odio, nec
-								consequat nisl tincidunt sit amet. Pellentesque
-								ac lacus pulvinar, condimentum orci sed, varius
-								nibh. Phasellus ac ligula a massa ultrices
-								dapibus. Pellentesque condimentum a purus nec
-								vehicula. Ut ac massa pulvinar, ullamcorper
-								libero id, suscipit risus. Maecenas volutpat
-								consectetur sapien, sed convallis sem hendrerit
-								porta. Morbi pharetra elit et nibh luctus
-								eleifend. Suspendisse et pellentesque enim.
-							</p>
-							<p>
-								Sed lectus leo, interdum in vestibulum sit amet,
-								elementum sed ipsum. Praesent volutpat turpis
-								nec enim malesuada, at volutpat lorem sagittis.
-								Integer sit amet enim dolor. Class aptent taciti
-								sociosqu ad litora torquent per conubia nostra,
-								per inceptos himenaeos. Curabitur faucibus nibh
-								ac dolor pulvinar, non ullamcorper ex vulputate.
-								Suspendisse aliquam eu sapien id laoreet.
-								Maecenas vitae arcu justo.
-							</p>
-							<p>
-								Praesent nisl est, sagittis quis tempor non,
-								pellentesque at urna. Proin pharetra gravida
-								rhoncus. Curabitur fermentum sem eget tortor
-								porttitor, non aliquam erat elementum.
-								Pellentesque habitant morbi tristique senectus
-								et netus et malesuada fames ac turpis egestas.
-								Quisque dignissim convallis urna ac hendrerit.
-								Donec viverra tincidunt nisi, in dapibus dui
-								scelerisque nec. Proin gravida odio sit amet
-								quam rutrum, sed imperdiet leo rutrum. Integer
-								eu mattis urna.
-							</p>
-							<Button
-								className="rounded-pill m-0"
-								style={styles.readMoreButton}
+
+		if (this.state.readFlag) {
+			renderData = data.map((d) => (
+				<span key={d.id}>
+					<hr
+						className="horizRule mb-5 mt-4 px-0 mx-0"
+						style={styles.horizRule}
+					/>
+					<Container>
+						<Row className="ml-2">
+							<Col
+								xs="12"
+								lg="1"
+								className="pr-2 px-0 text-lg-center"
 							>
-								<span>read more...</span>
-							</Button>
-						</Col>
-					</Row>
-				</Container>
-			</span>
-		));
+								<img
+									src="/images/Ellipse2.png"
+									style={styles.avatar}
+									className="mb-2"
+								/>
+							</Col>
+							<Col className="px-0">
+								<h4>My Father Patrick McCarthy's Garden</h4>
+								<h6>Posted By: Maria McCarthy</h6>
+								<h6>Date posted: June 10, 2020</h6>
+								<h6 className="mb-4">
+									Memories | Folklore | Father
+								</h6>
+							</Col>
+						</Row>
+						<Row className="ml-2 mr-5">
+							<Col xs="12" lg="11" className="px-0 offset-lg-1">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur
+									adipiscing elit. Vivamus velit ligula,
+									vehicula eu egestas at, bibendum eget
+									ligula. Mauris vehicula mattis ultrices.
+									Nulla auctor risus nulla, quis dictum nulla
+									semper nec. Curabitur nec orci ex. Integer
+									viverra augue odio, nec consequat nisl
+									tincidunt sit amet. Pellentesque ac lacus
+									pulvinar, condimentum orci sed, varius nibh.
+									Phasellus ac ligula a massa ultrices
+									dapibus. Pellentesque condimentum a purus
+									nec vehicula. Ut ac massa pulvinar,
+									ullamcorper libero id, suscipit risus.
+									Maecenas volutpat consectetur sapien, sed
+									convallis sem hendrerit porta. Morbi
+									pharetra elit et nibh luctus eleifend.
+									Suspendisse et pellentesque enim.
+								</p>
+								<p>
+									Sed lectus leo, interdum in vestibulum sit
+									amet, elementum sed ipsum. Praesent volutpat
+									turpis nec enim malesuada, at volutpat lorem
+									sagittis. Integer sit amet enim dolor. Class
+									aptent taciti sociosqu ad litora torquent
+									per conubia nostra, per inceptos himenaeos.
+									Curabitur faucibus nibh ac dolor pulvinar,
+									non ullamcorper ex vulputate. Suspendisse
+									aliquam eu sapien id laoreet. Maecenas vitae
+									arcu justo.
+								</p>
+								<p>
+									Praesent nisl est, sagittis quis tempor non,
+									pellentesque at urna. Proin pharetra gravida
+									rhoncus. Curabitur fermentum sem eget tortor
+									porttitor, non aliquam erat elementum.
+									Pellentesque habitant morbi tristique
+									senectus et netus et malesuada fames ac
+									turpis egestas. Quisque dignissim convallis
+									urna ac hendrerit. Donec viverra tincidunt
+									nisi, in dapibus dui scelerisque nec. Proin
+									gravida odio sit amet quam rutrum, sed
+									imperdiet leo rutrum. Integer eu mattis
+									urna.
+								</p>
+								<Button
+									className="rounded-pill m-0"
+									style={styles.readMoreButton}
+								>
+									<span>read more...</span>
+								</Button>
+							</Col>
+						</Row>
+					</Container>
+				</span>
+			));
+		} else if (this.state.writeFlag) {
+			renderData = <h1>This is the Write Screen!</h1>;
+		}
+
 		return (
 			<React.Fragment>
+				<Button onClick={this.toggleRead}>Read</Button>
+				<Button onClick={this.toggleWrite}>Write</Button>
 				<div className="pb-4">{renderData}</div>
 			</React.Fragment>
 		);
@@ -123,7 +155,7 @@ const styles = {
 	readMoreButton: {
 		fontFamily: "Comfortaa",
 		fontWeight: "400",
-		fontSize:"20px",
+		fontSize: "20px",
 		marginLeft: "8px",
 		width: "152px",
 		border: "none",
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("./../controllers/controllers");
 
-router.get("/say-something", controllers.saySomething);
+router.route("/say-something")
+    .get(controllers.saySomething);
 
 module.exports = router;
