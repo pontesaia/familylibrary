f09e844e9c34e44c2e08226b0d517b793f35a60f
Brandon Leon <bsleon@umich.edu>
2020-11-20 15:28:48 -0500

    New Build

---
+++ client/build/precache-manifest.5e91c452ad727d932cc74855d6870973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721ecfcca3e595e543d24024846e0290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31f19e60978b03eb3731",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "c020abe4fd762478bd2e",
+    "url": "/static/css/main.3bde1f19.chunk.css"
+  },
+  {
+    "revision": "31f19e60978b03eb3731",
+    "url": "/static/js/2.e463a38d.chunk.js"
+  },
+  {
+    "revision": "8d3e3a90ff83c10c3c25eae3acdbd6a1",
+    "url": "/static/js/2.e463a38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c020abe4fd762478bd2e",
+    "url": "/static/js/main.307b950b.chunk.js"
+  },
+  {
+    "revision": "51e9948600a4617966d5",
+    "url": "/static/js/runtime-main.e55976be.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29e228652d44ccba06e2b84b5d2f10ad.js"
+  "/precache-manifest.5e91c452ad727d932cc74855d6870973.js"
 );
 
 self.addEventListener('message', (event) => {
