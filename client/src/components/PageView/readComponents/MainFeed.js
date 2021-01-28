27be5714686ababa9635854a3f40396df81bb384
Brandon Leon <bsleon@umich.edu>
2021-01-27 19:45:13 -0500

    Change MainFeed to be a functional component

--- client/src/components/PageView/readComponents/MainFeed.js
+++ client/src/components/PageView/readComponents/MainFeed.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import axios from "axios";
 
-import StoryComponent from "./Story";
+import Story from "./Story";
 import PageViewLayout from "../PageViewLayout";
 
+function MainFeed() {
+	const [currentUserStory, setCurrentUserStory] = useState("");
+	const [mainFeedStoryFlag, setMainFeedStoryFlag] = useState(false);
+	const [userStories, setUserStories] = useState("");
+	const [titles, setTitles] = useState("");
+	const [stories, setStories] = useState("");
 
-class MainFeed extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { currentUserStory: "" };
-	}
-
-	trimString(str, length) {
+	const trimString = (str, length) => {
 		if (str && str.length > length)
 			return str.substring(0, length) + ".....";
 		else return str;
-	}
+	};
 
-	getStoryDate(i) {
+	const getStoryDate = (i) => {
 		let createdAt;
 		let date;
-		if (this.state.userStories) {
-			createdAt = this.state.userStories[i].createdAt;
-			date = JSON.stringify(new Date(createdAt).toDateString());
-			date = date.substring(1, date.length - 1);
-		}
+		createdAt = userStories[i].createdAt;
+		date = JSON.stringify(new Date(createdAt).toDateString());
+		date = date.substring(1, date.length - 1);
+		// }
 		return date;
-	}
+	};
 
-	getCurrentUserStory(story) {
-		this.setState({ currentUserStory: story });
-	}
+	const getCurrentUserStory = (story) => {
+		setCurrentUserStory(story);
+	};
 
-	getUserStories() {
-		//get userStories
+	const getUserStories = () => {
 		axios
 			.get("/userStories/")
 			.then((response) => {
 				if (response.data.length > 0) {
-					this.setState({
-						userStories: response.data,
-						titles: response.data.map(
-							(userStory) => userStory.title
-						),
-						stories: response.data.map(
-							(userStory) => userStory.story
-						),
-					});
+					setUserStories(response.data);
+					// setTitles(
+					// 	response.data.map((userStory) => userStory.title)
+					// );
+					// setStories(
+					// 	response.data.map((userStory) => userStory.story)
+					// );
 				}
 			})
 			.catch((error) => {
 				console.log(error);
 			});
-	}
+	};
 
-	componentDidMount() {
-		this.getUserStories();
-	}
+	useEffect(() => {
+		getUserStories();
+	});
 
-	render() {
-		let renderData = null;
-		if (this.state.userStories)
-			// if (!this.props.mainFeedStoryFlag) {
-			renderData = this.state.userStories.map((d, i) => (
-				<span key={this.state.userStories[i]._id}>
+	let renderData = userStories ? (
+		!mainFeedStoryFlag ? (
+			userStories.map((d, i) => (
+				<span key={userStories[i]._id}>
 					<hr
 						className="horizRule mb-5 mt-4 px-0 mx-0"
 						style={styles.horizRule}
@@ -77,7 +71,7 @@ class MainFeed extends Component {
 								className="pr-3 px-0 text-lg-center"
 							>
 								<img
-									src={this.state.userStories[i].avatar}
+									src={userStories[i].avatar}
 									style={styles.avatar}
 									className="mb-2"
 									alt="avatar"
@@ -88,18 +82,18 @@ class MainFeed extends Component {
 								<h6>
 									<b>Posted By:</b>{" "}
 								</h6>
-								<h6>{this.state.userStories[i].author}</h6>
+								<h6>{userStories[i].author}</h6>
 								<h6>
 									<b>Date posted: </b>
 								</h6>
-								<h6>{this.getStoryDate(i)}</h6>
+								<h6>{getStoryDate(i)}</h6>
 								<h6 className="mb-4">{d.tags}</h6>
 							</Col>
 							<Col xs="12" lg="7" className="pl-0 mb-2 pr-4">
 								<div
 									dangerouslySetInnerHTML={{
-										__html: this.trimString(
-											this.state.userStories[i].story,
+										__html: trimString(
+											userStories[i].story,
 											450
 										),
 									}}
@@ -108,10 +102,8 @@ class MainFeed extends Component {
 									className="rounded-pill m-0"
 									style={styles.readMoreButton}
 									onClick={() => {
-										this.getCurrentUserStory(
-											this.state.userStories[i]
-										);
-										this.props.setMainFeedStory(true);
+										getCurrentUserStory(userStories[i]);
+										setMainFeedStoryFlag(true);
 									}}
 								>
 									<span>read more...</span>
@@ -126,24 +118,20 @@ class MainFeed extends Component {
 						</Row>
 					</Container>
 				</span>
-			));
-		// } else if (this.props.mainFeedStoryFlag) {
-		// 	renderData = (
-		// 		<StoryComponent
-		// 			currentUserStory={this.state.currentUserStory}
-		// 		/>
-		// 	);
-		// }
-		return (
-			<React.Fragment>
-				<PageViewLayout body={renderData} />
-			</React.Fragment>
-		);
-	}
+			))
+		) : (
+			<Story currentUserStory={currentUserStory} />
+		)
+	) : null;
+
+	return (
+		<React.Fragment>
+			<PageViewLayout body={renderData} />
+		</React.Fragment>
+	);
 }
 
 const styles = {
-
 	avatar: {
 		width: "clamp(10px, 100%, 105px)",
 		borderRadius: "50%",
