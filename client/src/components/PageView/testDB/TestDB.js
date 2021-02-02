3209ae281af136d588aff681b2d037fbf85814af
Brandon Leon <bsleon@umich.edu>
2021-02-02 14:11:07 -0500

    convert TestDB to functional

--- client/src/components/PageView/testDB/TestDB.js
+++ client/src/components/PageView/testDB/TestDB.js
@@ -3,42 +3,37 @@ import { Container, Button } from "reactstrap";
 import axios from "axios";
 import DB from "./testDB.json";
 
-class TestDB extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	loadDB = () => {
+const TestDB = () => {
+	const loadDB = () => {
 		// e.preventDefault();
 		// const userStory = {
 		// 	title: this.state.title,
 		// 	story: this.state.story,
 		// };
 		// console.log(userStory);
-        // window.location = "/PageView";
-        console.log(DB);
-        console.log(typeof DB)
-		axios.post("/userStories", DB.userstories).then((res) => console.log(res.data));
+		// window.location = "/PageView";
+		console.log(DB);
+		console.log(typeof DB);
+		axios
+			.post("/userStories", DB.userstories)
+			.then((res) => console.log(res.data));
 	};
 
-	deleteDB = () => {
+	const deleteDB = () => {
 		axios.delete("/userStories").then((res) => {
 			console.log(res.data);
 		});
 	};
 
-	render() {
-		console.log(DB);
-		return (
-			<React.Fragment>
-				<Container className="mt-5 pt-5 text-center">
-					<Button onClick={this.deleteDB}>Delete DB</Button>
-					<Button onClick={this.loadDB}>Load DB</Button>
-				</Container>
-			</React.Fragment>
-		);
-	}
-}
+	console.log(DB);
+	return (
+		<React.Fragment>
+			<Container className="mt-5 pt-5 text-center">
+				<Button onClick={deleteDB}>Delete DB</Button>
+				<Button onClick={loadDB}>Load DB</Button>
+			</Container>
+		</React.Fragment>
+	);
+};
 
 export default TestDB;
