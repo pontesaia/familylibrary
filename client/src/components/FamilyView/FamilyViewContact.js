dbffecaca599428c16fa77d21675bcbdc028fdc3
Brandon Leon <bsleon@umich.edu>
2021-02-09 18:28:31 -0500

    Redo FamilyView Animation

--- client/src/components/FamilyView/FamilyViewContact.js
+++ client/src/components/FamilyView/FamilyViewContact.js
@@ -1,11 +1,19 @@
 import React from "react";
 import FamilyViewLayout from "./FamilyViewLayout";
 import Contact from "../Home/Contact";
+import Fade from "react-reveal/Fade";
+
 
 const FamilyViewContact = () => {
 	return (
 		<React.Fragment>
-			<FamilyViewLayout body={<Contact />} />
+			<FamilyViewLayout
+				body={
+					<Fade bottom duration={500}>
+						<Contact />
+					</Fade>
+				}
+			/>
 		</React.Fragment>
 	);
 };
--- client/src/components/FamilyView/FamilyViewFeatures.js
+++ client/src/components/FamilyView/FamilyViewFeatures.js
@@ -1,11 +1,18 @@
 import React from "react";
 import FamilyViewLayout from "./FamilyViewLayout";
 import Features from "../Home/Features";
+import Fade from "react-reveal/Fade";
 
 const FamilyViewFeatures = () => {
 	return (
 		<React.Fragment>
-			<FamilyViewLayout body={<Features />} />
+			<FamilyViewLayout
+				body={
+					<Fade bottom duration={500}>
+						<Features />
+					</Fade>
+				}
+			/>
 		</React.Fragment>
 	);
 };
