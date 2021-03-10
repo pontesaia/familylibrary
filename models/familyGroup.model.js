6291999096e40d003da8340b3d470a811317833e
Brandon Leon <bsleon@umich.edu>
2021-03-09 20:35:16 -0500

    Make familyGroupRoutes delete all, update server familyGroup app.use

--- models/familyGroup.model.js
+++ models/familyGroup.model.js
@@ -6,7 +6,7 @@ const familyGroupSchema = new Schema(
 		groupName: { type: String, required: true },
 		groupAdmin: { type: Array, required: false },
 		groupMembers: { type: Array, required: false },
-		groupZip: { type: String, required: false },
+		// groupZip: { type: String, required: false },
 	},
 	{ timestamps: true }
 );
